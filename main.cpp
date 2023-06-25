3eaca5ebf4c817269b60958e7c6a1976c3cacd40
T-Ageishi <takuto.ageishi.dev@gmail.com>
2023-06-25 19:01:01 +0900

    add files

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ typedef unsigned long long ull;
 typedef long long ll;  // 9*10^18
 const long long LINF = 1LL << 60;
 const int INF = 2000000000;
+/* MAIN ************************************************** */
 
 /**
  * main
