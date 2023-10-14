7279dc70b88953df532f362d687d71e1bf277756
T-Ageishi <takuto.ageishi.dev@gmail.com>
2023-10-14 14:09:18 +0900

    ファイルを削除したり

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # AtCoder
 AtCoder
 
-# 学び
-* Map
-  http://vivi.dyndns.org/tech/cpp/map.html
+# online-judge-tools
+## サンプルケースのダウンロード
+oj d https://atcoder.jp/contests/agc001/tasks/agc001_a
+
+## コンパイル
+g++ main.cpp
+
+## テスト
+oj t
--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define rrep(i, a, b) for (int i = a; i >= b; i--)
+// #include <atcoder/all>
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
+// using namespace atcoder;
 typedef unsigned long long ull;
 typedef long long ll;  // 9*10^18
-const ll infl = 1LL << 60;
-const int inf = INT_MAX / 2;
 #define rep(i, a, b)  for (int i = a; i < b; i++)
 #define rrep(i, a, b) for (int i = a; i >= b; i--)
-using mint = modint998244353;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
 /******************************************************** */
 // int gcd(int A, int B) {
 //   while (A >= 1 && B >= 1) {
